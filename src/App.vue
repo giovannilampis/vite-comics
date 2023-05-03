bc0a4bbb6500834d710893697a3478d345fececb
giovannilampis <giolampis@hotmail.com>
2023-05-03 17:01:35 +0200

    add basic content to header

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,50 @@
 </script>
 
 <template>
+
+  <div class="container d-flex">
+
+    <div>
+        <img src="../public/img/dc-logo.png" alt="logo">
+    </div>
+
+    <ul class="text-uppercase d-flex">
+      
+      <a href=""><li>characters</li></a>
+      
+      <a href=""><li>comics</li></a>
+      
+      <a href=""><li>movies</li></a>
+      
+      <a href=""><li>tv</li></a>
+      
+      <a href=""><li>games</li></a>
+      
+      <a href=""><li>collectibles</li></a>
+      
+      <a href=""><li>videos</li></a>
+      
+      <a href=""><li>fans</li></a>
+      
+      <a href=""><li>news</li></a>
+
+      <a href=""><li>shop</li></a>
+      
+    </ul>
+
+  </div>
  
 </template>
 
 <style lang="scss">
+
+  a {
+    text-decoration: none;
+  }
+
+  li {
+    color: red;
+    list-style-type: none;
+  }
 
 </style>
