152a352fad679a908b829a5a19131e09d92ca28d
giovannilampis <giolampis@hotmail.com>
2023-05-04 19:37:04 +0200

    improve .blue-menu layout

--- src/components/BlueSection.vue
+++ src/components/BlueSection.vue
@@ -7,43 +7,43 @@
 </script>
 
 <template>
-    
+
     <div class="blue-menu">
 
-        <div class="container-blue">
+        <div class="container-blue d-flex justify-content-between align-items-center ">
 
             <ul class="d-flex text-uppercase">
 
                 <li class="d-flex justify-content-between align-items-center">
-                    <div>
+                    <div class="d-flex justify-content-center align-items-center">
                         <img src="/public/img/buy-comics-digital-comics.png" alt="digital-comics-buy">
                     </div>
                     <a href="#">digital comics</a>
                 </li>
 
                 <li class="d-flex justify-content-between align-items-center">
-                    <div>
+                    <div class="d-flex justify-content-center align-items-center">
                         <img src="/public/img/buy-comics-merchandise.png" alt="comics-merchandise">
                     </div>
                     <a href="#">dc merchandise</a>
                 </li>
 
                 <li class="d-flex justify-content-between align-items-center">
-                    <div>
+                    <div class="d-flex justify-content-center align-items-center">
                         <img src="/public/img/buy-comics-subscriptions.png" alt="comics-subscriptions">
                     </div>
                     <a href="#">subscription</a>
                 </li>
 
-                <li class="d-flex justify-content-between align-items-center">
-                    <div>
+                <li class="d-flex justify-content-center align-items-center">
+                    <div class="d-flex justify-content-between align-items-center">
                         <img src="/public/img/buy-comics-shop-locator.png" alt="shop-locator">
-                    </div>  
+                    </div>
                     <a href="#">comic shop locator</a>
                 </li>
 
                 <li class="d-flex justify-content-between align-items-center">
-                    <div>
+                    <div class="d-flex justify-content-center align-items-center">
                         <img src="/public/img/buy-dc-power-visa.svg" alt="dc-power-visa">
                     </div>
                     <a href="">dc power visa</a>
@@ -66,6 +66,7 @@
 
     .blue-menu {
         height: 8rem;
+        line-height: 8rem;
         background-color: rgba(2, 130, 249, 1);
     }
     .container-blue {
@@ -78,9 +79,17 @@
         aspect-ratio: 1 / 1;
     }
 
+    ul {
+        margin: 0 auto;
+    }
+
     li {
         list-style-type: none;
         margin-right: 3rem;
+    }
+
+    ul li:last-child {
+        margin-right: 0;
     }
 
     a {
